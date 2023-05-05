6289fbdc0e146882b589b586fd58b0800cde8c8f
TamaraCliment <t.climentfuentes@edu.gva.es>
2023-05-05 18:06:34 +0200

    Test y ejercicios

---
+++ ejercicio.js.js
@@ -0,0 +1,13 @@
+/* Función par */
+var par = function par (a) {
+	return ( a % 2 ) == 0;	
+};
+/* Función suma */
+var suma = function suma (a, b) {
+	return a + b;	
+};
+
+/* Función resta */
+var resta = function suma (a, b) {
+	return a - b;	
+};
